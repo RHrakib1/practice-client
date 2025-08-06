d68f28c578fa97e40ad76269eabcf1727112d5bb
RHrakib1 <rakibhasanmd457@gmail.com>
2025-08-06 22:31:30 +0600

    creat practice form

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,22 @@ import './App.css'
 
 function App() {
 
+  const hendlefrom = event => {
+    event.preventDefault()
+    const shortData = event.target
+    const name = shortData.name.value
+    const email = shortData.email.value
+    const dataObj = { name, email }
+    console.log("from a data add hoyese", dataObj)
+  }
+
   return (
     <>
-      <h1>hi</h1>
+      <form onSubmit={hendlefrom}>
+        <input type="text" name='name' placeholder='enter your name' /><br /><br />
+        <input type="email" name='email' placeholder='enter your email' /><br /><br />
+        <input type="submit" value='Go to database' />
+      </form>
     </>
   )
 }
