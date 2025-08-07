7802837b0a33d904f46915ba75954f5a2e4698e6
RHrakib1 <rakibhasanmd457@gmail.com>
2025-08-07 08:54:03 +0600

    deletet method successfully

--- Datauser.jsx
+++ Datauser.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import { useLoaderData } from 'react-router-dom'
 
+const hendeldelete = id => {
+    console.log('delete hbe ai id ta ', id)
+    fetch(`http://localhost:5001/pruser/${id}`, {
+        method: "DELETE"
+    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+
+            if (data.deletedCount > 0) {
+                alert('delete done')
+            }
+        })
+}
+
 export default function Datauser() {
     const dataload = useLoaderData()
     return (
         <div>
             <h1>data number:{dataload.length}</h1>
             {
-                dataload.map(data => <p>{data.email} : {data.name}</p>)
+                dataload.map(data => <p>{data._id}-------{data.email} : {data.name}
+                    <button onClick={() => hendeldelete(data._id)}>X</button></p>)
             }
         </div>
     )
