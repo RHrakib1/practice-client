97e7fb5f55574b97cd73ba24e0b13b2f6b43c578
RHrakib1 <rakibhasanmd457@gmail.com>
2025-08-08 09:29:56 +0600

    add to the single update method

--- Update.jsx
+++ Update.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import { useLoaderData } from 'react-router-dom'
+import { data, useLoaderData } from 'react-router-dom'
 
 export default function Update() {
     const loaddata = useLoaderData()
+    const hendleclick = e => {
+        e.preventDefault()
+        const datastore = e.target
+        const name = datastore.name.value
+        const email = datastore.email.value
+        const dataobj = { name, email }
+        console.log(dataobj)
+    }
     return (
         <div>
-            <input type="text" name='name' defaultValue={loaddata.name} /><br /><br />
-            <input type="email" name='email' defaultValue={loaddata.email} /><br /><br />
-            <input type="submit" value='Update data' />
+            <form onSubmit={hendleclick}>
+                <input type="text" name='name' defaultValue={loaddata.name} /><br /><br />
+                <input type="email" name='email' defaultValue={loaddata.email} /><br /><br />
+                <input type="submit" value='Update data' />
+            </form>
         </div>
     )
 }
